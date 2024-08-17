d6f0d75c320b1d4bdb9e1ce277bd693cfe1c5477
Rashid Rashidov <rrashidov@gmail.com>
2024-08-17 17:16:37 +0300

    Fix lock controller annotation

--- api/src/main/java/org/roko/dls/api/controller/LockController.java
+++ api/src/main/java/org/roko/dls/api/controller/LockController.java
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/lock")
+@RestController
+@RequestMapping("/api/v1/lock")
 public class LockController {
 
     private LockService svc;
